106e070559a77e82cb06226307f6b48db57d7a9c
Viktor Nagy <viktor.nagy1995@gmail.com>
2024-05-30 20:56:29 +0200

    feat: debug log

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -10,6 +10,7 @@ export default async function Home() {
 	const files = userId === undefined
 		? await api.file.getAll()
 		: await api.file.getByUserId(userId);
+	console.log(userId);
 	return (
 		<main className="flex min-h-screen flex-col items-center justify-center ">
 			<div className="container flex flex-col items-center justify-center gap-12 px-4 py-16 ">
